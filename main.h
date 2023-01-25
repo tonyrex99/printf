8ca71ecafdba2771be5f8c72580aab068cfebaa1
Glory Adaeze Esinulo <daeze7@gmail.com>
2023-01-25 05:34:21 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
 #include <stdarg.h>
+
 void _putchar(char);
 int _strlen(char *);
 int _abs(int d);
@@ -12,4 +14,5 @@ char *handle_specifier(char specifier, va_list va);
 char get_specifier(char *s);
 int _puts(char *);
 int _printf(const char *format, ...);
+
 #endif
